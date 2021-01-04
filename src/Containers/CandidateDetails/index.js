3459050263559c7d65cf4f1a0bcabad1b1545334
Sumit Rajput <sumitrajput7488@gmail.com>
2021-01-04 21:25:18 +0530

    Integrated search functionality

--- src/Containers/CandidateDetails/index.js
+++ src/Containers/CandidateDetails/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const CandidateDetails = () => {
+const CandidateDetails = (props) => {
   return (
     <div>CandidateDetails</div>
   );
--- src/Containers/Home/index.js
+++ src/Containers/Home/index.js
@@ -1,27 +1,34 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Row, Col, Avatar, Card, Input } from 'antd';
+import { withRouter } from 'react-router';
 import './Home.css';
-import { withRouter } from "react-router";
 
 const { Search } = Input;
-
 const { Meta } = Card;
 
 const Home = ({ candidates, history }) => {
-  console.log(candidates)
+  const [searchedText, updateSearchedText] = useState('');
+  const [filteredList, setFilteredList] = useState([]);
+
+  useEffect(() => {
+    const list = candidates.filter(
+      candidate => candidate.name.toLowerCase().includes(searchedText.toLowerCase())
+    );
+    setFilteredList(list)
+  }, [candidates, searchedText])
   return (
     <>
       <Search
-        placeholder="input search text"
+        placeholder="Enter Candidate Name"
         allowClear
         enterButton="Search"
         size="large"
         style={{ width: 320, margin: '10px 10px' }}
-        onSearch={(text) => console.log(text)}
+        onSearch={(text) => updateSearchedText(text)}
       />
       <Row gutter={[16, 24]}>
         {
-          candidates.map(candidate => (
+          filteredList.map(candidate => (
             <Col key={candidate.id} className="gutter-row" span={6}>
               <Card onClick={() => history.push(`/${candidate.id}`)} style={{ width: 250, marginTop: 16 }} hoverable>
                 <Meta
